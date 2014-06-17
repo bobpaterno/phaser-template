d09ecba0ca05e256765f38ef84bf0c4f36c3fbfd
Robert Paterno <bobpaterno@gmail.com>
2014-06-16 21:13:59 -0500

    even simpler template yet

---
+++ app/static/js/es6/main.es6.js
@@ -0,0 +1,19 @@
+/* global Game, Phaser */
+
+var game;
+
+(function(){
+  'use strict';
+
+  $(document).ready(init);
+
+  function init(){
+    game = new Phaser.Game(800, 600, Phaser.AUTO, 'game');
+
+    game.state.add('Boot', Game.Boot);
+    game.state.add('Load', Game.Load);
+
+    game.state.start('Boot');
+  }
+
+})();
