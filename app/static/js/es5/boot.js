93ce14a7df44611e2ac3d53a8cf55bd0254b6a1e
Robert Paterno <bobpaterno@gmail.com>
2014-06-16 21:16:16 -0500

    uber basic template

--- app/static/js/es5/boot.js
+++ app/static/js/es5/boot.js
@@ -4,18 +4,6 @@
 
 var Game = {};
 
-// initialize variables
-var w = 800;
-var h = 600;
-
-var grid = [];
-var columns = 13;
-var rows = 13;
-
-var Directions = { Up: 0, Right: 1, Down: 2, Left: 3 };
-
-var hexColors = ['#aaccff', '#aaffcc', '#ccffaa', '#ccaaff', '#ffccaa', '#ffaacc'];
-
 Game.Boot = function(game) {
    this.game = game;
 };
